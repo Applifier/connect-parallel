0a98f59e52c133579896bfe79d3cf5c7c495bec2
Matti Savolainen <matti@applifier.com>
2012-05-03 09:10:21 +0300

    test it with and without errors.

--- test/test.connect-parallel.js
+++ test/test.connect-parallel.js
@@ -1,21 +1,21 @@
 var parallel = process.env.EXPRESS_COV ? require('../lib-cov/connect-parallel') : require('../lib/connect-parallel');
 var assert = require('assert');
 
-exports.testParallel = function(test) {
+exports.testParallel = function (test) {
   var req = {foo:[]};
   var res = {};
   parallel([
-  function(req, res, next){
-    setTimeout(function(){
-      req.foo.push("foo");
+    function (req, res, next) {
+      setTimeout(function () {
+        req.foo.push("foo");
+        next();
+      }, 30);
+    },
+    function (req, res, next) {
+      req.foo.push("bar");
       next();
-    }, 30);
-  },
-  function(req, res, next){
-    req.foo.push("bar");
-    next();
-  }
-  ])(req, res, function(){
+    }
+  ])(req, res, function () {
     assert.equal(req.foo[0], "bar");
     assert.equal(req.foo[1], "foo");
     test();
@@ -23,39 +23,54 @@ exports.testParallel = function(test) {
 };
 
 
-exports.testParallelWithError = function(test) {
+exports.testParallelWithError = function (test) {
   var req = {foo:[]};
   var res = {};
   parallel([
-  function(req, res, next){
-    setTimeout(function(){
-      req.foo.push("foo");
-      next();
-    }, 30);
-  },
-  function(req, res, next){
-    next(new Error("foobar"));
-  }
-  ])(req, res, function(err){
+    function (req, res, next) {
+      setTimeout(function () {
+        req.foo.push("foo");
+        next();
+      }, 30);
+    },
+    function (req, res, next) {
+      next(new Error("foobar"));
+    }
+  ])(req, res, function (err) {
     assert.equal(err.message, "foobar");
     test();
   });
 };
 
-exports.testWithArguments = function(test) {
+exports.testWithArguments = function (test) {
   var req = {foo:[]};
   var res = {};
   parallel(
-  function(req, res, next){
-    setTimeout(function(){
-      req.foo.push("foo");
-      next();
-    }, 30);
-  },
-  function(req, res, next){
-    next(new Error("foobar"));
-  })(req, res, function(err){
+    function (req, res, next) {
+      setTimeout(function () {
+        req.foo.push("foo");
+        next();
+      }, 30);
+    },
+    function (req, res, next) {
+      next(new Error("foobar"));
+    })(req, res, function (err) {
     assert.equal(err.message, "foobar");
-    test();
+    parallel(
+      function (req, res, next) {
+        setTimeout(function () {
+          req.foo.push("foo");
+          next();
+        }, 30);
+      },
+      function (req, res, next) {
+        req.foo.push("bar");
+        next();
+      }
+    )(req, res, function () {
+      assert.equal(req.foo[0], "bar");
+      assert.equal(req.foo[1], "foo");
+      test();
+    });
   });
 };
